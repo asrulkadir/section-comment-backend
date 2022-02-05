efe4bed9df1a7fedd650338fd867a9fe2e3284da
asrulkadir <asrulkadir.asus4@gmail.com>
2022-02-05 09:01:01 +0800

    comment database

--- main.go
+++ main.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
+	// "database/sql"
+	// "fmt"
+	// "log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	_ "github.com/lib/pq"
 
-	"github.com/asrulkadir/section-comment-backend/comments"
-	"github.com/asrulkadir/section-comment-backend/config"
+	// "github.com/asrulkadir/section-comment-backend/comments"
+	// "github.com/asrulkadir/section-comment-backend/config"
 	"github.com/asrulkadir/section-comment-backend/pkg/validator"
 )
 
 func main() {
-	configViper, err := config.InitViper()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// configViper, err := config.InitViper()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		configViper.Database.DBUser,
-		configViper.Database.DBPassword,
-		configViper.Database.DBHost,
-		configViper.Database.DBPort,
-		configViper.Database.DBName,
-	)
+	// dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+	// 	configViper.Database.DBUser,
+	// 	configViper.Database.DBPassword,
+	// 	configViper.Database.DBHost,
+	// 	configViper.Database.DBPort,
+	// 	configViper.Database.DBName,
+	// )
 
-	conn, err := sql.Open("postgres", dsn)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// conn, err := sql.Open("postgres", dsn)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	if err := conn.Ping(); err != nil {
-		panic(err)
-	} else {
-		fmt.Println("DB Connected...")
-	}
+	// if err := conn.Ping(); err != nil {
+	// 	panic(err)
+	// } else {
+	// 	fmt.Println("DB Connected...")
+	// }
 
 	e := echo.New()
 	validator := validator.New()
@@ -52,13 +52,13 @@ func main() {
 	})
 
 	//repository
-	commentRepo := comments.NewRepositoryComments(conn)
+	// commentRepo := comments.NewRepositoryComments(conn)
 
-	//service
-	commentService := comments.NewServiceComment(commentRepo)
+	// //service
+	// commentService := comments.NewServiceComment(commentRepo)
 
-	//controller
-	comments.NewController(e, commentService, validator)
+	// //controller
+	// comments.NewController(e, commentService, validator)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
